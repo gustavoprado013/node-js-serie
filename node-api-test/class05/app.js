892b3af53b8fd9989e98af48425e0f0b0934777d
Gustavo Prado <guprado013@gmail.com>
2016-10-31 21:11:39 -0200

    Alterações

---
+++ node-api-test/class05/app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import config from './config/config';
+import datasource from './config/datasource'
+
+const app = express();
+app.config = config;
+app.datasource = datasource(app);
+
+app.set('port', 3442);
+
+const Books = app.datasource.models.Books;
+
+app.route('/books')
+  .get((req, res) => {
+    Books.findAll({})
+      .then(result => res.json(result))
+      .catch(err => res.status(412));
+});
+
+export default app;
